8551ca3326521be1c5dbd8394af3ef0c33ff75e4
Evangelos Giataganas <e.giataganas@gmail.com>
2020-05-08 14:51:31 +0100

    Create an Elastic IP associated with EC2 instance

--- main.tf
+++ main.tf
@@ -10,3 +10,9 @@ resource "aws_instance" "my_ec2_instance" {
   ami           = "ami-01a6e31ac994bbc09"
   instance_type = "t2.micro"
 }
+
+# Create an Elastic IP associated with EC2 instance
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.my_ec2_instance.id
+}
