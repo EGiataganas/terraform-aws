777913f790cc946696a1d9425297105c4a1ca01d
Evangelos Giataganas <e.giataganas@gmail.com>
2020-05-08 14:44:25 +0100

    Create an EC2 instance and run terraform apply

--- main.tf
+++ main.tf
@@ -4,3 +4,9 @@ provider "aws" {
   region  = "eu-west-2"
   profile = "default"
 }
+
+# Create an EC2 instance
+resource "aws_instance" "my_ec2_instance" {
+  ami           = "ami-0596aab74a1ce3983"
+  instance_type = "t2.micro"
+}
